f5d8c9b6081e449651044c99065e5f3c059bc501
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-05-20 08:12:43 +0930

    220520 Commit
    220520 Commit

---
+++ 8kyu-Sum-the-Strings.js
@@ -0,0 +1,34 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Tests example test cases", () => {
+    assert.strictEqual(sumStr("4","5"), "9");
+    assert.strictEqual(sumStr("34","5"), "39");
+  });
+});
+
+
+// my code
+
+function sumStr(a,b) {
+    return (Number(a)+Number(b)).toString()
+    
+  }
