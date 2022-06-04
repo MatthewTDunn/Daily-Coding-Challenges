b233573774df3257d279f80957702bf0dba0a74e
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-06-04 10:15:45 +0930

    220604 Commit
    220604 Commit

---
+++ 8kyu-If-you-cant-sleep-count-sheep.js
@@ -0,0 +1,27 @@
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Fixed tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(countSheep(0), "");
+    assert.strictEqual(countSheep(1), "1 sheep...");
+    assert.strictEqual(countSheep(2), "1 sheep...2 sheep...");
+    assert.strictEqual(countSheep(3), "1 sheep...2 sheep...3 sheep...");
+  });
+});
+
+// my code
+
+var countSheep = function (num){
+    let returnString = ""
+    for (let i=1;i<=num;i++){
+      returnString = returnString + `${i} sheep...`
+    }
+    return returnString
+  }
