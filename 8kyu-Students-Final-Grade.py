19e93702eb442c09695d54055b15f29830b7ed60
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-06-12 20:41:15 +0930

    220612 Commit
    220612 Commit

---
+++ 8kyu-Students-Final-Grade.py
@@ -0,0 +1,44 @@
+# Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+# This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+# This function should return a number (final grade). There are four types of final grades:
+
+# 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+# 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+# 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+# 0, in other cases
+# Examples(Inputs-->Output):
+
+# 100, 12 --> 100
+# 99, 0 --> 100
+# 10, 15 --> 100
+
+# 85, 5 --> 90
+
+# 55, 3 --> 75
+
+# 55, 0 --> 0
+# 20, 2 --> 0
+# *Use Comparison and Logical Operators.
+
+import codewars_test as test
+from solution import final_grade
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(final_grade(100, 12), 100)
+        test.assert_equals(final_grade(85, 5), 90)
+
+        # my code
+        
+def final_grade(exam, projects):
+    if exam>90 or projects>10:
+        return 100
+    if exam>75 and projects>=5:
+        return 90
+    if exam>50 and projects>=2:
+        return 75
+    return 0
