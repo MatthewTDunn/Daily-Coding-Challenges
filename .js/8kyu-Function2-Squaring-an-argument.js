33fcf7e5a8d10391d45d3e281e4d30d974faaac6
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-06-19 06:31:08 +0930

    220619 Commit
    220619 Commit

---
+++ .js/8kyu-Function2-Squaring-an-argument.js
@@ -0,0 +1,13 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+describe("Tests", () => {
+    it("test", () => {
+  Test.assertEquals(square(3), 9);
+  });
+  });
+
+  // my code
+
+  function square(num) {
+    return num*num
+  }
