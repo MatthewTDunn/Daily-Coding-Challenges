99c7851c58f32c87f59385a30458cf8af3faa222
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-06-28 07:31:20 +0930

    220628 Commit
    220628 Commit

---
+++ .js/7kyu-Round-up-to-the-next-multiple-of-5.js
@@ -0,0 +1,38 @@
+// Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    [[0,0],[1,5],[-1,0],[-5,-5],[3,5],[5,5],[7,10],[20,20],[39,40],[990,990],[121,125],[555,555]].forEach(
+      ([x,exp])=> assert.strictEqual(roundToNext5(x), exp, `Input: ${x}`)
+    );
+  })
+});
+
+
+// my code
+
+function roundToNext5(n){
+    const ceilNumber = Math.ceil(n/5)
+    return ceilNumber*5
+  }
