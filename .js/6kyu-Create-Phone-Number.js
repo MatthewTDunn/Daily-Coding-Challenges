baf06d066030e20298d5c729d6268873c578b12a
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-08-03 06:49:22 +0930

    220803 Commit
    220803 Commit

---
+++ .js/6kyu-Create-Phone-Number.js
@@ -0,0 +1,29 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+// Don't forget the space after the closing parenth
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Create Phone Number", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+    assert.strictEqual(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+  });
+});
+
+// my code
+
+// 
+// @param: a 10 index array consisting of integers
+// @return: this array returned in a string, telephone format
+// 
+
+function createPhoneNumber(numbers){
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers.slice(3,6).join("")}-${numbers.slice(6,numbers.length).join('')}`  
+}
