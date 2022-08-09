258244a6cc5bd8c1aef929ae34cc35ae8dbde985
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-08-10 06:52:09 +0930

    220810 Commit
    220810 Commit

---
+++ .js/8kyu-5-without-numbers.js
@@ -0,0 +1,17 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+describe("unusualFive", function(){
+    it("should return 5", function(){
+      assert.strictEqual(unusualFive(), 5);
+    });
+  });
+
+// my code
+
+function unusualFive() {
+    return [,,,,,].length
+}
