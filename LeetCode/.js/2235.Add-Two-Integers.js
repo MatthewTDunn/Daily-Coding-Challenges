ddbde1731a4b7604e3c102c060995754e09761a5
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-09-14 07:13:58 +0930

    220914 Commit
    220914 Commit

---
+++ LeetCode/.js/2235.Add-Two-Integers.js
@@ -0,0 +1,34 @@
+// Given two integers num1 and num2, return the sum of the two integers.
+ 
+
+// Example 1:
+
+// Input: num1 = 12, num2 = 5
+// Output: 17
+// Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+// Example 2:
+
+// Input: num1 = -10, num2 = 4
+// Output: -6
+// Explanation: num1 + num2 = -6, so -6 is returned.
+ 
+
+// Constraints:
+
+// -100 <= num1, num2 <= 100
+
+// my code
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+ var sum = function(num1, num2) {
+    return num1+num2
+};
+
+// code comparison
+
+// Runtime: 126 ms, faster than 5.20% of JavaScript online submissions for Add Two Integers.
+// Memory Usage: 42.1 MB, less than 52.01% of JavaScript online submissions for Add Two Integers.
