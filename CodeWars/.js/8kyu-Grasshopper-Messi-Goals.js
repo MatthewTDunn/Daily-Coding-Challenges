665e2ca50c25ca308df26b1dd23dd3fdf1b8211e
Matthew Dunn <97732029+Pyr1te@users.noreply.github.com>
2022-10-04 07:03:29 +1030

    221004 Commit
    221004 Commit

---
+++ CodeWars/.js/8kyu-Grasshopper-Messi-Goals.js
@@ -0,0 +1,32 @@
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+describe("total goals", function(){
+    it ("should equal the total", function () {
+      Test.assertEquals(totalGoals, 58)
+    })
+})
+
+// my code
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals
+
+totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
