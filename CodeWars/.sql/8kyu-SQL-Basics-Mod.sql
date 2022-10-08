0466c96b1f37e7ca5e9d62013a8a523766cd6c04
Matthew Dunn <97732029+MatthewTDunn@users.noreply.github.com>
2022-10-09 08:34:38 +1030

    221009 Commit
    221009 Commit

---
+++ CodeWars/.sql/8kyu-SQL-Basics-Mod.sql
@@ -0,0 +1,38 @@
+-- Given the following table 'decimals':
+
+-- ** decimals table schema **
+
+-- id
+-- number1
+-- number2
+-- Return a table with one column (mod) which is the output of number1 modulus number2.
+
+results = run_sql
+
+describe :query do
+  describe "should contain keywords" do
+    it "should contain SELECT" do
+      expect($sql.upcase).to include("SELECT")
+    end
+
+    it "should contain FROM" do
+      expect($sql.upcase).to include("FROM")
+    end
+  end
+
+  describe :columns do
+    it "should return 1 column" do
+      expect(results.first.keys.count).to eq 1
+    end
+    
+    it "should return a mod column" do
+      expect(results.first.keys).to include(:mod)
+    end
+ end
+end
+
+-- my code
+
+/*  SQL  */
+
+SELECT MOD(number1,number2) FROM decimals
