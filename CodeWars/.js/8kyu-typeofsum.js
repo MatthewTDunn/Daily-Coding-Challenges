931ab5b151d631668d4556578f202ea239ada1c0
Matthew Dunn <97732029+MatthewTDunn@users.noreply.github.com>
2023-04-06 06:15:24 +0930

    230406

---
+++ CodeWars/.js/8kyu-typeofsum.js
@@ -0,0 +1,20 @@
+// Return the type of the sum of the two arguments
+
+describe("Basic tests",() => {
+    Test.assertEquals(typeOfSum(12, 1), 'number');
+    Test.assertEquals(typeOfSum('d', 1), 'string');
+    Test.assertEquals(typeOfSum(1, 'a'), 'string');
+    Test.assertEquals(typeOfSum('dd', ''), 'string');
+    Test.assertEquals(typeOfSum(true, 1), 'number');
+    Test.assertEquals(typeOfSum('s', false), 'string');
+    Test.assertEquals(typeOfSum(null, 1), 'number');
+    Test.assertEquals(typeOfSum('s', null), 'string');
+    Test.assertEquals(typeOfSum(null, undefined), 'number');
+    Test.assertEquals(typeOfSum(undefined, 're'), 'string');
+    Test.assertEquals(typeOfSum(undefined, true), 'number');
+    Test.assertEquals(typeOfSum(null, false), 'number');
+  })
+
+  function typeOfSum(a, b) {
+    return typeof(a+b)
+  }
