acbe8acb19ee1bdec99c57d8fec55a1b04971189
Matthew Dunn <97732029+MatthewTDunn@users.noreply.github.com>
2023-09-20 05:31:48 +0930

    230920

---
+++ CodeWars/.cs/8kyu-ConvertBooleanValuesToStrings.cs
@@ -0,0 +1,33 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class  boolToWordTest {
+
+  [Test]
+  public void boolToWordReturned1() {
+    Assert.AreEqual("Yes", Kata.boolToWord(true));
+    Console.WriteLine("Expected Yes");
+  }
+
+  [Test]
+  public void boolToWordReturned2() {
+    Assert.AreEqual("No", Kata.boolToWord(false));
+    Console.WriteLine("Expected No");
+  }
+
+  [Test]
+  public void boolToWordReturned3() {
+    Assert.AreEqual("Yes", Kata.boolToWord(true));
+    Console.WriteLine("Expected Yes");
+  }
+}
+
+// my code
+
+public static class Kata
+{
+  public static string boolToWord(bool word) => (word == true) ? "Yes" : "No";
+}
