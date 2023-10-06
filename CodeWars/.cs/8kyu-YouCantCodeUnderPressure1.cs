0585a3055d744bab62852f01eb9edb56967e7295
Matthew Dunn <97732029+MatthewTDunn@users.noreply.github.com>
2023-10-07 03:40:22 +1030

    231007

---
+++ CodeWars/.cs/8kyu-YouCantCodeUnderPressure1.cs
@@ -0,0 +1,35 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+  [TestFixture]
+  public class CodeWarsTest
+  {
+    private static IEnumerable<TestCaseData> testCases
+    {
+      get
+      {
+        yield return new TestCaseData(2).Returns(4);
+        yield return new TestCaseData(4).Returns(8);
+        yield return new TestCaseData(-10).Returns(-20);
+        yield return new TestCaseData(0).Returns(0);
+        yield return new TestCaseData(100).Returns(200);
+      }
+    }
+  
+  	[Test, TestCaseSource("testCases")]
+    public int Test(int n) => Kata.DoubleInteger(n);
+  }
+}
+
+// my code
+
+public static class Kata
+{
+  public static int DoubleInteger(int n) => n*2;
+}
+    
